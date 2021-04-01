7611b8cd9c3d3f71c9aa1841ff9b29b8af32b589
Marcelo Block Teixeira <marcelochb@gmail.com>
2021-04-01 13:36:48 -0300

    feat: Infity scrool list

--- lib/src/components/organisms/cardList/cardList_widget.dart
+++ lib/src/components/organisms/cardList/cardList_widget.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:pocket/src/models/card_model.dart';
+import 'package:pocket/src/components/organisms/cardList/cardlist_controller.dart';
 import 'package:pocket/src/components/molecules/cardListItem/cardListItm_widget.dart';
 import 'package:pocket/src/theme/color_theme.dart';
 
@@ -13,44 +15,65 @@ class CardListWidget extends StatefulWidget {
 }
 
 class _CardListWidgetState extends State<CardListWidget> {
-  List<ModelOfCard> cards = [];
-  final cardRepository = CardRepository();
-  bool isLoading = true;
+  final cardListController = CardListController();
+  final ScrollController _scrollController = new ScrollController();
 
   loadData() async {
     try {
-      List<ModelOfCard> _cards = await cardRepository.fetchCards();
-      setState(() {
-        cards = _cards;
-        isLoading = false;
-      });
-    } catch (e) {
-      print('error do widget $e');
-      setState(() {
-        isLoading = false;
-      });
-    }
+      await cardListController.getCards();
+    } catch (e) {}
   }
 
   @override
   void initState() {
-    super.initState();
     this.loadData();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        this.loadData();
+      }
+    });
+    super.initState();
+  }
+
+  disposed() {
+    _scrollController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (isLoading)
-      return SpinKitRotatingCircle(
-        color: ColorTheme.instance.element,
-        size: 50.0,
+    return Observer(builder: (_) {
+      if (cardListController.isLoading)
+        return SpinKitRotatingCircle(
+          color: ColorTheme.instance.element,
+          size: 50.0,
+        );
+      return Stack(
+        children: [
+          ListView.builder(
+              itemCount: cardListController.cards.length,
+              controller: _scrollController,
+              addAutomaticKeepAlives: false,
+              dragStartBehavior: DragStartBehavior.down,
+              itemBuilder: (context, index) {
+                return CardListItemWidget(
+                  item: cardListController.cards[index],
+                );
+              }),
+          if (cardListController.isLoadingCardListItem)
+            Container(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                SpinKitHourGlass(
+                  color: ColorTheme.instance.background,
+                  size: 50.0,
+                ),
+              ],
+            )),
+        ],
       );
-    return ListView.builder(
-        itemCount: cards.length,
-        itemBuilder: (context, index) {
-          return CardListItemWidget(
-            item: cards[index],
-          );
-        });
+    });
   }
 }
--- lib/src/repositories/card_repository.dart
+++ lib/src/repositories/card_repository.dart
@@ -2,13 +2,20 @@ import 'package:dio/dio.dart';
 import 'package:pocket/src/models/card_model.dart';
 
 class CardRepository {
-  final dio = Dio();
-  final url = 'https://api.magicthegathering.io/v1/cards';
+  final _dio = Dio();
+  final _url = 'https://api.magicthegathering.io/v1/cards';
+  final _pageLimitByRequest = 3;
 
-  Future<List<ModelOfCard>> fetchCards() async {
+  Future<List<ModelOfCard>> fetchCards({int initialPage = 1}) async {
     List<ModelOfCard> cards = [];
     try {
-      final response = await dio.get(url);
+      final response = await _dio.get(
+        _url,
+        queryParameters: {
+          'page': initialPage,
+          'pageSize': _pageLimitByRequest,
+        },
+      );
       final list = response.data['cards'] as List;
       for (var json in list) {
         final card = ModelOfCard.fromJson(json);
@@ -25,7 +32,7 @@ class CardRepository {
   Future<ModelOfCard> fetchCardById(String id) async {
     ModelOfCard card;
     try {
-      final response = await dio.get('$url/$id');
+      final response = await _dio.get('$_url/$id');
       card = ModelOfCard.fromJson(response.data['card']);
     } catch (e) {
       print('Error api $e');
